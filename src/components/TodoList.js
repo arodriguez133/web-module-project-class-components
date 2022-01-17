a2d46431777bb547b0f822c2b03804db51670eda
arodriguez133 <arodriguez133@icloud.com>
2022-01-17 15:52:52 -0600

    mapped the todo's from state to the Todo items through props

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,2 +1,17 @@
+import React from 'react';
+
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+
+
+//This is where I have to map the items within the Todo Item. 
+export default class TodoList extends React.Component{
+    //I don't need contructor or super just the render method
+    render(props){
+        return (
+            {props.todoItems.map(item => {
+                <Todo key={item.id} item={item.todo}/>
+            })}
+        )
+    }
+}
