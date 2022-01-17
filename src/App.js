5089659170a7e010fb64075a8ccad1bde988a46f
arodriguez133 <arodriguez133@icloud.com>
2022-01-17 14:59:33 -0600

    created initial data called 'todoItems' then added constructor and super. I initialized stated and assigned todoItems to it. Then passed it down through the TodoList component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import React from 'react';
 import TodoList from './components/TodoList'
 
+ const todoItems = [{
+  todo: "Get Groceries",
+  id: 61241,
+  completed: false
+},
+{
+  todo: "Cook Dinner",
+  id: 61242, 
+  completed: false,
+}]
 class App extends React.Component {
+constructor(){
+  super();
+
+  this.state = {
+    todo: todoItems,
+  }
+}
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+
   render() {
     return (
       <div>
-        <TodoList />
+        <TodoList todoItems={this.state.todo} />
       </div>
     );
   }
